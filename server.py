31d20591c2498d2e637750d6b9e6fd8254758331
SagiKerner <sagikerner9@gmail.com>
2017-03-28 19:34:27 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,13 +2,26 @@ from flask import Flask
 app = Flask(__name__)
 from functools import wraps
 from flask import request, Response
+import csv
 
 
 def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    return username == 'dmin' and password == 'secret'
+    with open('users.csv') as users_file:
+        reader = csv.reader(users_file)
+        users = {}
+        for row in reader:
+            key = row[0]
+            if key in users:
+                # implement your duplicate row handling here
+                pass
+            users[key] = row[1:]
+    if username in users:
+        if users[username][0] == password:
+            return True
+    return False
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
